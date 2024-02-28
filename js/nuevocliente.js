76717d0c98ca9f7889cefe241b97fc3e2f3af2e5
anJu0703 <jpmantilla2000@hotmail.com>
2024-02-28 14:08:52 -0500

    Pasando info a BD

--- js/nuevocliente.js
+++ js/nuevocliente.js
@@ -47,6 +47,38 @@
             imprimirAlerta('Todos los campos son obligatorios','error');
             return;
         }
+
+        //Crear un objeto con la informacion
+        const cliente = {
+            nombre,
+            email,
+            telefono,
+            empresa,
+            id: Date.now()
+        };
+
+        crearNuevoCliente(cliente);
+    };
+
+    function crearNuevoCliente(cliente) {
+        const transaction = DB.transaction(['crm'],'readwrite');
+
+        const objectStore = transaction.objectStore('crm');
+
+        objectStore.add(cliente);
+
+        transaction.onerror = function () {
+            imprimirAlerta('Hubo un error','error');
+        };
+
+        transaction.oncomplete = function () {
+            
+            imprimirAlerta('El cliente se agrego correctamente');
+
+            setTimeout(() => {
+                window.location.href = 'index.html';    
+            }, 3000);
+        };
     };
 
 
